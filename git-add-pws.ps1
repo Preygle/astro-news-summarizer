6e3dd8d1f72d4862b5fb39cfe2b449eb35533b34
Preygle <mohammad.owais2004@gmail.com>
2025-06-10 16:37:04 +0530

    added ps1 command to add git files

---
+++ git-add-pws.ps1
@@ -0,0 +1,16 @@
+
+# main directory
+git add .gitignore
+git add requirements.txt
+git add news_scrape.py
+git add news_summarize.py
+git add web_ui.py
+git add astronomy_summaries_falconsai.json
+git add README.md
+
+# api_integration directory
+git add api_integration/news_summarizer_api.py
+git add api_integration/requirements.txt
+
+
+git status
